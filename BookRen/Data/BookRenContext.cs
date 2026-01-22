8730be63f483497a1140278eba43dd38bca58e29
REN BOL <renat13bolurov@gmail.com>
2026-01-22 19:50:30 +0300

    Added a DB with EF

---
+++ BookRen/Data/BookRenContext.cs
@@ -0,0 +1,16 @@
+﻿using BookRen.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRen.Data
+{
+    public class BookRenContext : DbContext
+    {
+        public BookRenContext(DbContextOptions<BookRenContext> options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; } = default!;
+    }
+}
---
+++ BookRen/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookRen.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+
+    }
+}
